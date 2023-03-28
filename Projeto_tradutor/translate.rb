4ac60ebc04f3d6a1bbd4388fd62bb6de8aabedec
Matheus M Almeida <matheus.mma2015@gmail.com>
2023-03-28 18:30:40 -0300

    criando o tradutor

--- Projeto_tradutor/translate.rb
+++ Projeto_tradutor/translate.rb
@@ -1,21 +1,21 @@
 require 'net/http'
 require 'google/cloud/translate'
 
-project_id = "projecttranslete"
-credentials = "AIzaSyDhnL0tCpRmi3YZVJwscskWe-3dPLy0QJo"
+project_id = 'projecttranslete'
+credentials = 'AIzaSyDhnL0tCpRmi3YZVJwscskWe-3dPLy0QJo'
 
 translate = Google::Cloud::Translate.new project: project_id, credentials: credentials
 
-puts '-------Tradutor-------'
-puts 'DUgite o texto que deseja ser traduzido: '
+puts '----Tradutor----'
+puts 'Digite o que deseja traduzir: '
 text = gets.chomp
 
-puts 'Digite o idioma de origem: (ex: en):'
+puts 'Digite o idioma de origem: (ex: en) '
 source_language = gets.chomp
 
-puts 'Digite o idioma que deseja traduzir: (ex: pt):'
+puts 'Digite o idioma para qual deve ser traduzido: (ex: pt) '
 translate_language = gets.chomp
 
 translation = translate.translate text, to: translate_language, from: source_language
 
-puts "Tradução #{translation.text}"
+puts "Traduzido #{"Traduzido: #{translation}"}"
